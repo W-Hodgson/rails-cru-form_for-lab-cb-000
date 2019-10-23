7ade1052ab6378e4364e6801fe7f5698fa493824
Will Hodgson <will.hodgson@current-rms.com>
2019-10-23 12:04:20 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,9 +22,10 @@ class SongsController < ApplicationController
   end
 
   def edit
+    @song = Song.find(params[:id])
     @genre = Genre.find(@song.genre_id)
     @artist = Artist.find(@song.artist_id)
-    @song = Song.find(params[:id])
+
   end
 
   def update
